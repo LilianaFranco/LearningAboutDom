57693fa6c47beed7b2f61586d5a04f6b53c610c5
LilianaFranco <franco0123@gmail.com>
2022-08-14 22:50:17 -0500

    Eliminar elements del DOM. No se cómo guardarlos antes de eliminarlos.

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ let seeDark = confirm("¿Deseas ver esta página en modo oscuro?")
 if (seeDark) {
         document.querySelector('body').classList.add('darkMode')   
 }
+
+let posts = document.getElementsByClassName("item");
+console.log(posts)
+console.log(posts[0])
+
+while (posts.length>0){
+        posts[0].remove();
+}
+
+console.log(posts)
+
