e8324adee9cf9cf6810a75c66c682f63e1aa598e
LilianaFranco <franco0123@gmail.com>
2022-08-15 20:47:22 -0500

    Creación de un arreglo para guardar los datos del HTML. Los selectores solo traen la referencia, ese era el problema de borrado.

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
+//capturar el botón para crearle un listener
 const goDark = document.getElementById("boton-dark")
 console.log(goDark);
 
+//Escuchar el clic de un botón para que funcione el dark mode cuando el usuario desee.
 goDark.addEventListener("click", function (){
         document.querySelector('body').classList.toggle("darkMode") 
 })
 
+//Obtener los post de felinos y crear arreglo para guardarlos
 let posts = document.getElementsByClassName("item");
-let savedPosts = posts;
-console.log(posts)
-console.log(posts[0])
+let felinos = []
 
-// while (posts.length>0){
-//         posts[0].remove();
-// }
+//Agregar los datos del HTML en un array de felinos
+for (let i = 0; i < posts.length; i++) {
+        let imagenURL = posts[i].firstElementChild.getAttribute("src")
+        let titulo = posts[i].querySelector("h2").innerText
+        let parrafo = posts[i].querySelector("p").innerText
+        felinos.push({imagenURL, titulo, parrafo})
+}
 
-console.log(savedPosts)
+//Borrar posts de la pantalla
+console.log(felinos)
+while (posts.length>0){
+        posts[0].remove();
+}
 
+// Agregar los posts de nuevo al HTML usando una plantilla
+for (let index = 0; index < felinos.length; index++) {
+        const felino = felinos[index];
+        createPost(felino.imagenURL, felino.titulo, felino.parrafo)
+}
+
+//Plantilla para crear posts de felinos
 function createPost(imagen, titulo, parrafo){
         const postTemplate = `
                 <div class="item">
@@ -27,20 +43,3 @@ function createPost(imagen, titulo, parrafo){
         document.querySelector(".contenedor").innerHTML += postTemplate;
 }
 
-console.log(savedPosts)
-
-
-//prueba
-let imagenURL = savedPosts[0].firstElementChild.getAttribute("src")
-let titulo = savedPosts[0].querySelector("h2").innerText
-let parrafo = savedPosts[0].querySelector("p").innerText
-createPost(imagenURL, titulo, parrafo)
-
-//Si uso este for se crea dinámicamente y se crea un loop infinito.
-
-// for (let i = 0; i < savedPosts.length; i++) {
-//         let imagenURL = savedPosts[i].firstElementChild.getAttribute("src")
-//         let titulo = savedPosts[i].querySelector("h2").innerText
-//         let parrafo = savedPosts[i].querySelector("p").innerText
-//         createPost(imagenURL, titulo, parrafo)
-// }
